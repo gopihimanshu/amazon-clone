c5dfcece7e6f92b3652a1203b4118219974dae8f
Himanshu <gopihimanshu@gmail.com>
2020-09-11 23:33:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header.js";
+
+function App() {
+  return (
+    <div className="app">
+      {/* <h1>Hello Clever Programmer, lets build the Amazon Clone</h1> */}
+      <Header />
+      {/* Home */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBdXtUeOr9FpFNST-7DDBclzsFXaFZGivM",
+    authDomain: "clone-810f8.firebaseapp.com",
+    databaseURL: "https://clone-810f8.firebaseio.com",
+    projectId: "clone-810f8",
+    storageBucket: "clone-810f8.appspot.com",
+    messagingSenderId: "225925714267",
+    appId: "1:225925714267:web:7b4c38c1e90b0218e7ca7e"
+  };
