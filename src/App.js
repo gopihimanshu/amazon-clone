b27f823b38f1a3e3c337b7e59262e0806ea4b53a
Himanshu <gopihimanshu@gmail.com>
2020-09-13 16:55:00 +0530

    Finished the home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header.js";
+import Home from "./Home.js";
 
 function App() {
   return (
     <div className="app">
-      {/* <h1>Hello Clever Programmer, lets build the Amazon Clone</h1> */}
       <Header />
-      {/* Home */}
+      <Home />
     </div>
   );
 }
