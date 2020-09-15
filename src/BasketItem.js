a39f933b10eda2e76f0d31169f112e75b16d95d6
Himanshu <gopihimanshu@gmail.com>
2020-09-16 01:07:52 +0530

    Added checkout basket functionality

---
+++ src/BasketItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./BasketItem.css";
+import { useStateValue } from "./StateProvider";
+
+function BasketItem() {
+  const [{ basket }, dispatch] = useStateValue();
+  let basketItem;
+  basket.map((item) => (basketItem = item));
+  let { title, price, rating } = basketItem;
+  return (
+    <div className="basket">
+      <p>{title}</p>
+      <p className="basket_price">
+        <small>$</small>
+        <strong>{price}</strong>
+      </p>
+      <div className="basket__rating">
+        {Array(rating)
+          .fill()
+          .map((_, i) => (
+            <p>⭐</p>
+          ))}
+      </div>
+    </div>
+  );
+}
+
+export default BasketItem;
